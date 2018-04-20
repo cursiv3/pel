bd67f45588649f79ef1d2dbf95de4c44717aa81a
Corey <corey@corey-laptop.hersheykiss>
2018-04-20 14:47:08 -0700

    api scaffolding in place, need to start testing

--- primeedge_api/app.js
+++ primeedge_api/app.js
@@ -3,22 +3,50 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
+// jwt secret
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// ===================== CORS =========================================
+app.all("*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  if ("OPTIONS" === req.method) {
+    //respond with 200
+    res.sendStatus(200);
+  } else {
+    //move on
+    next();
+  }
+});
+
+app.options("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  res.send(200);
+});
+
+// ====================================================================
+
+app.use("/", routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,8 +60,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  res.status(err.status || 500).json({ message: err.message, error: err });
 });
 
 module.exports = app;
---
+++ primeedge_api/routes/index.js
@@ -0,0 +1,125 @@
+const routes = require("express").Router();
+const promise = require("bluebird");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+const options = {
+  promiseLib: promise
+};
+const pgp = require("pg-promise")(options);
+const db = pgp(config.database);
+
+routes.get("/", (req, res) => {
+  res.send("api server functioning");
+});
+
+// ****************************************************************************
+//  check for duplicate entry on registration
+// ****************************************************************************
+routes.get("/checkDupes", (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+
+  db
+    .task("check-dupes", async DB => {
+      return [
+        await DB.oneOrNone(`SELECT username FROM users WHERE username = $1`, [
+          username
+        ]),
+        await DB.oneOrNone(`SELECT email FROM users WHERE email = $1`, [email])
+      ];
+    })
+    .then(userList => {
+      console.log(userList);
+      if (userList[0] != null) {
+        res.json({ success: false, message: "Username already exists." });
+      } else if (userList[1] != null) {
+        res.json({
+          success: false,
+          message: "Email address already in use."
+        });
+      } else {
+        return true;
+      }
+    });
+});
+
+// ****************************************************************************
+//  save user to database
+// ****************************************************************************
+
+routes.post("/saveUser", (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let saltRounds = 10;
+
+  bcrypt.hash(req.body.password, saltRounds, (err, hashPass) => {
+    db
+      .none("INSERT INTO users(username, pword, email) VALUES($1, $2, $3)", [
+        username,
+        hashPass,
+        email
+      ])
+      .then(data => {
+        console.log("User saved successfully!");
+        res.json({
+          success: true,
+          message: "user submitted"
+        });
+      })
+      .catch(error => {
+        console.log("failed, error: " + error);
+        res.json({ success: false, err: error });
+      })
+      .catch(err => {
+        console.log(err);
+        res.json({ success: false, message: "Server 500 error" });
+      });
+  });
+});
+
+// ****************************************************************************
+// login submit
+// ****************************************************************************
+routes.post("/submitLogin", (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  db
+    .any("SELECT * FROM users WHERE username = $1", [username])
+    .then(user => {
+      if (user.length < 1) {
+        res.json({
+          success: false,
+          message: "User does not exist."
+        });
+      } else {
+        var usr = user[0].username;
+        var hashPass = user[0].pword;
+
+        bcrypt.compare(password, hashPass, (err, doesPwMatch) => {
+          if (doesPwMatch != true || usr != username) {
+            res.json({
+              success: false,
+              message: "Username or password incorrect."
+            });
+          } else {
+            const payload = { user: usr };
+            var token = jwt.sign(payload, config.secret, {
+              expiresIn: 60 * 60 * 24
+            });
+            console.log("Successfully created token!");
+            res.json({ success: true, message: "Token created", token: token });
+          }
+        });
+      }
+    })
+    .catch(error => {
+      console.log("CATCH ERROR: " + error);
+      res.json({ success: false, err: error });
+    });
+});
+
+module.exports = routes;
